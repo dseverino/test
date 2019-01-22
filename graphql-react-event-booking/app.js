d923213457450d87e8c8fd9c776daa3bb2ede6e2
dseverino <domingoseverino30@gmail.com>
2019-01-22 08:59:23 -0400

    Refactor split resolvers

--- graphql-react-event-booking/app.js
+++ graphql-react-event-booking/app.js
@@ -20,9 +20,7 @@ app.use("/graphql",
 
 //const url = `mongodb://localhost:27017/${process.env.MONGO_DB}`;
 const url = `mongodb://dseverino:${process.env.MONGO_PASSWORD}@node-rest-shop-shard-00-00-cqreh.mongodb.net:27017,node-rest-shop-shard-00-01-cqreh.mongodb.net:27017,node-rest-shop-shard-00-02-cqreh.mongodb.net:27017/${process.env.MONGO_DB}?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true`
-
-mongoose.connect(url, {useMongoClient: true})
-
 mongoose.Promise = global.Promise;
+mongoose.connect(url, {useMongoClient: true})
 
 app.listen(3000)
